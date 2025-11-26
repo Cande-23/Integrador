5ac022f4588d2d105236ecc739007e22fc57d43c
Cande <130178549+Cande-23@users.noreply.github.com>
2025-11-26 01:29:56 -0300

    ExamenMercadoDs2025Application.java

--- src/main/java/com/utn/ExamenMercado_DS2025/ExamenMercadoDs2025Application.java
+++ src/main/java/com/utn/ExamenMercado_DS2025/ExamenMercadoDs2025Application.java
@@ -1,13 +1,13 @@
 package com.utn.ExamenMercado_DS2025;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ExamenMercadoDs2025Application {
-
     public static void main(String[] args) {
-        SpringApplication.run(ExamenMercadoDs2025Application.class, args);
+        SpringApplication.run(ExamenMercadoDs2025Application.class, args); 
     }
-
 }
